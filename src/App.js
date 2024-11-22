4469949dc683ecd2bfae1780045d895434d07a49
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2024-11-22 15:01:04 +0530

    change style files position

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import router from './routes';
 import './styles/index.scss';
-import './styles/theme/theme.scss';
+import './styles/_theme.scss';
 
 function App() {
   return (
